b0d4b73658aa15a949fc9109dd283422a3207448
Brandon Ashby <69142310+Braash@users.noreply.github.com>
2022-02-08 08:55:03 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,21 +4,7 @@ import Coins from '../components/Coins'
 import Footer from '../components/Footer'
 
 const Home: React.FC = () => {
-    /*
-    const handleClick = (e: any) => {
-        const id = e.target.id;
-        console.log(id);
-        console.log(e.target)
-        axios
-          .get(SingleCoin(id))
-          .then(res => {
-            setIndvCoins(res.data);
-            console.log(res.data);
-            console.log(indvCoins)
-          })
-          .catch(error => console.log(error));
-    };
-    */
+   
     return (
         <div>
             <Coins />
